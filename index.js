73b925e4b53a8645748fbe0703fef54c40603c8a
Carl Eiserman <carleiserman@gmail.com>
2017-08-16 11:13:54 +0200

    feature: experimental promise request update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
  */
 
 let request = require("request");
+let requester = require("request-promise");
 
 /**
  * Sends a message once supplied with secrets file.
@@ -14,46 +15,54 @@ let request = require("request");
  * @param message_body
  */
 function sendMessage(secrets, message_number, message_body) {
-    // Messagegate sends the entire object through as a url param.
-    let options = {
-        method: 'POST',
-        url: (function(){
+    return new Promise((resolve, reject) => {
+        // Messagegate sends the entire object through as a url param.
+        let options = {
+            method: 'POST',
+            url: (function(){
 
-            let params = {
-                "username": secrets.MESSAGEGATE.username,
-                "password": secrets.MESSAGEGATE.password,
-                "account": secrets.MESSAGEGATE.account,
-                "da": parseNumber(message_number),
-                "ud": message_body,
-                "id": "message02",
-            };
+                let params = {
+                    "username": secrets.MESSAGEGATE.username,
+                    "password": secrets.MESSAGEGATE.password,
+                    "account": secrets.MESSAGEGATE.account,
+                    "da": parseNumber(message_number),
+                    "ud": message_body,
+                    "id": "message02",
+                };
 
-            let mainUrl = `http://sms.msgm8.mobi/submit/single/?username=${params.username}`;
+                let mainUrl = `http://sms.msgm8.mobi/submit/single/?username=${params.username}`;
 
-            // Build the url from the params we've entered,
-            // ignoring the username as it always gets added first
-            for (let i in params){
-                if (i !== "username"){
-                    mainUrl += `&${i}=${params[i]}`
+                // Build the url from the params we've entered,
+                // ignoring the username as it always gets added first
+                for (let i in params){
+                    if (i !== "username"){
+                        mainUrl += `&${i}=${params[i]}`
+                    }
                 }
-            }
 
 
-            return mainUrl;
-        })(),
-        body: (function() {
-            return { }
-        })(),
-        headers: {
-            "Content-Type": "application/json",
-        },
-        json: true
-    };
+                return mainUrl;
+            })(),
+            body: (function() {
+                return { }
+            })(),
+            headers: {
+                "Content-Type": "application/json",
+            },
+            json: true
+        };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        console.log("LOGGING RESPONSE");
-        console.log(body);
+        // requester(options, function (error, response, body) {
+        //     if (error) throw new Error(error);
+        //     console.log("LOGGING RESPONSE");
+        //     console.log(body);
+        // });
+
+        requester(options).then(response => {
+            console.log(response);
+        }).catch(ex => {
+            console.log("Error!", ex);
+        })
     });
 }
 
